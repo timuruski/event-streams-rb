4df359e83d6aab04776bde2d6ec67e15ab46f247
Tim Uruski <hey@tim.uru.ski>
2021-04-02 13:04:05 -0600

    Add minimal Topic concept

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Event Streams
 
 This is a repo for playing with event streams in Ruby.
+
+Stream (sequence of events) -> Topic (subset of events) -> Subscription (topic + handler)
+
+Stream, linear sequence of events, handles de-duplication.
+
+Topic, selects a subset of events, handles subscribing and publishing.
+Is the label part of the event or separate?
+How to handle aggregate topics though? Subscribing merges events, but how does publishing work?
+Are events published multiple times? The topic labels are preserved in the stream.
+
+topic(a, b) -> publish(event) -> stream((event, a, b))
+  topic(a) -> receive(event, a)
+  topic(a) -> publish(event, a)
+  topic(a, b) -> receive(event, a)
+  topic(b) -> receive(event, a)
+  topic(a, b) -> receive(event, a)
+
+Subscription, combines a handler with a topic.
+
+Handler, receives events and routes them to a state.
--- example.rb
+++ example.rb
@@ -4,36 +4,97 @@
 # Subscription -> Handler + Topic
 
 at_exit do
+  stream = EventStream.new
+
   alice_greets_bob = Event.new("Alice greets Bob")
   alice_enters = Event.new("Alice enters")
   alice_leaves = Event.new("Alice leaves")
   bob_enters = Event.new("Bob enters")
   bob_leaves = Event.new("Bob leaves")
 
-  stream = EventStream.new
-  stream.publish_each(alice_enters, bob_enters)
+  all_events = Topic.new(stream)
+  all_events.publish_each(alice_enters, bob_enters)
+  all_events.publish_each(alice_greets_bob)
+  all_events.publish_each(bob_leaves, alice_leaves)
 
   handler_a = EventHandler.new { |event| puts "A: #{event.data}" }
   handler_b = EventHandler.new { |event| puts "B: #{event.data}" }
   handler_c = EventHandler.new { |event| puts "C: #{event.data}" }
 
-  stream.subscribe!(handler_a, last_event: nil)
-  stream.subscribe!(handler_b, last_event: alice_enters).unsubscribe
-  stream.subscribe!(handler_c)
+  all_events.subscribe!(handler_a, last_event: nil)
+  all_events.subscribe!(handler_b, last_event: alice_enters).unsubscribe
+  all_events.subscribe!(handler_c)
 
-  stream.publish_each(alice_greets_bob, bob_leaves, alice_leaves)
-  stream.publish Event.new("End of scene")
+  all_events.publish_each(alice_greets_bob, bob_leaves, alice_leaves)
+  all_events.publish Event.new("End of scene")
+end
+
+class Event
+  ID_GENERATOR = (1..).each
+
+  attr_reader :data, :id
+
+  def initialize(data)
+    @id = Event::ID_GENERATOR.next
+    @data = data
+  end
 end
 
 class EventStream
   def initialize
-    @subscriptions = []
+    @listeners = []
     @published_events = {}
     @ordered_events = []
   end
 
+  def subscribe(listener)
+    @listeners.push(listener)
+  end
+
+  def unsubscribe(listener)
+    @listeners.delete(listener)
+  end
+
+  def publish(event)
+    return event if @published_events[event.id]
+
+    @published_events[event.id] = @ordered_events.length
+    @ordered_events.push(event)
+
+    @listeners.each do |listener|
+      listener.deliver(event)
+    end
+
+    event
+  end
+
+  def each
+    @ordered_events.each do |event|
+      yield event
+    end
+  end
+
+  def each_after(last_event)
+    starting_index = @published_events[last_event.id] + 1
+    missing_events = @ordered_events.slice(starting_index..)
+    missing_events.each do |event|
+      yield event
+    end
+  end
+end
+
+class Topic
+  attr_reader :stream
+
+  def initialize(stream)
+    @stream = stream
+    @subscriptions = []
+
+    @stream.subscribe(self)
+  end
+
   def subscribe(handler, last_event: nil)
-    subscription = Subscription.new(stream: self, handler: handler)
+    subscription = Subscription.new(topic: self, handler: handler)
     @subscriptions.push(subscription)
 
     subscription
@@ -60,39 +121,62 @@ class EventStream
   end
 
   def publish(event)
-    @published_events[event.id] = @ordered_events.length
-    @ordered_events.push(event)
-
-    @subscriptions.each do |subscription|
-      subscription.deliver(event)
-    end
+    @stream.publish(event)
 
     event
   end
 
   def each
-    @ordered_events.each do |event|
+    @stream.each do |event|
       yield event
     end
   end
 
   def each_after(last_event)
-    starting_index = @published_events[last_event.id] + 1
-    missing_events = @ordered_events.slice(starting_index..)
-    missing_events.each do |event|
+    @stream.each_after(last_event) do |event|
       yield event
+    end
+  end
+
+  def deliver(event)
+    @subscriptions.each do |subscription|
+      subscription.deliver(event)
     end
   end
 end
 
-class Event
-  ID_GENERATOR = (1..).each
+class Subscription
+  attr_reader :handler, :topic
 
-  attr_reader :data, :id
+  def initialize(handler:, topic:)
+    @handler = handler
+    @topic = topic
+  end
 
-  def initialize(data)
-    @id = Event::ID_GENERATOR.next
-    @data = data
+  def deliver(event)
+    @handler.handle(event)
+
+    self
+  end
+
+  def play_after(last_event)
+    @topic.each_after(last_event) do |event|
+      deliver(event)
+    end
+
+    self
+  end
+
+  def play_all
+    @topic.each do |event|
+      deliver(event)
+    end
+
+    self
+  end
+
+  def unsubscribe
+    @topic.unsubscribe(self)
   end
 end
 
@@ -106,37 +190,3 @@ class EventHandler
   end
 end
 
-class Subscription
-  attr_reader :handler, :stream
-
-  def initialize(handler:, stream:)
-    @handler = handler
-    @stream = stream
-  end
-
-  def deliver(event)
-    @handler.handle(event)
-
-    self
-  end
-
-  def play_after(last_event)
-    @stream.each_after(last_event) do |event|
-      deliver(event)
-    end
-
-    self
-  end
-
-  def play_all
-    @stream.each do |event|
-      deliver(event)
-    end
-
-    self
-  end
-
-  def unsubscribe
-    @stream.unsubscribe(self)
-  end
-end
